4c2f946402e61ebc2b0069b0621870abcf689df4
hargren <nhargre1@hotmail.com>
2024-05-13 08:34:25 -0700

    added new online state

--- Sensor-Client/src/sensor-nhargrex/src/main.rs
+++ Sensor-Client/src/sensor-nhargrex/src/main.rs
@@ -6,7 +6,7 @@
 // export GOOGLE_APPLICATION_CREDENTIALS="/opt/.security/sensors-nhargrex-firebase-adminsdk-uev2w-11471882b8.json"
 // export GOOGLE_USER_ID="2U0LR6A8LER430Tq4tmdfAdl4iu2"
 // export GOOGLE_PROJECT_ID=sensors-nhargrex
-// cargo build && cargo run
+// sudo rm /tmp/sensor-nhargrex.log && cargo build && cargo run
 // cargo build --release
 // sudo systemctl daemon-reload && sudo systemctl start sensor-nhargrex && sudo systemctl status sensor-nhargrex
 //
@@ -42,7 +42,15 @@ pub enum State {
 struct SensorRefreshRequestObject {
     #[serde(alias = "_firestore_id")]
     doc_id: String,
-    r_ts: u64
+    r_ts: u64,
+    r_cmd: i32
+}
+
+// Sensor Request Firestore Document
+#[derive(Debug, Clone, Deserialize, Serialize)]
+struct SensorObject {
+    onine: bool,
+    state: String
 }
 
 lazy_static! {
@@ -57,7 +65,7 @@ const DEBOUNCE_TIME : Duration = Duration::from_millis(500);
 const POLLING_DURATION : Duration = Duration::from_millis(5000);
 const SENSORS_REFRESH_REQUEST_COLLECTION: &str = "sensorsRefreshRequest";
 const SENSORS_REFRESH_REQUEST_DOCUMENT_ID: FirestoreListenerTarget = FirestoreListenerTarget::new(17_u32);
-const REFRESH_REQUEST_TIMEWINDOW : i64 = -30;
+const REFRESH_REQUEST_TIMEWINDOW_SECONDS : i64 = -5;
 
 // Main
 #[tokio::main]
@@ -66,6 +74,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // statup log
     log_to_file("/tmp/sensor-nhargrex.log", LevelFilter::Info).unwrap();
     log::info!("Normal start");
+
+    log::info!("Wait to start");
+    thread::sleep(POLLING_DURATION * 6);
+    log::info!("Continuing");
     
     // setup firestore
     let db = FirestoreDb::with_options_service_account_key_file(
@@ -142,24 +154,65 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 FirestoreListenEvent::DocumentChange(ref doc_change) => {
                     if let Some(doc) = &doc_change.document {
                         let sensor_refresh_request: SensorRefreshRequestObject = FirestoreDb::deserialize_doc_to::<SensorRefreshRequestObject>(doc).expect("Deserialized object");
-                        log::info!("Recevied: {sensor_refresh_request:?} r_ts={}", sensor_refresh_request.r_ts);
-                        let delta_ts = (Utc.timestamp_opt(sensor_refresh_request.r_ts as i64, 0).unwrap() - Utc::now())
-                                        .num_seconds();
+                        log::info!("Recevied: {sensor_refresh_request:?} r_ts={}, r_cmd={}", sensor_refresh_request.r_ts, sensor_refresh_request.r_cmd);
+                        let delta_ts = (Utc.timestamp_opt(sensor_refresh_request.r_ts as i64, 0).unwrap() - Utc::now()).num_seconds();
                         log::info!("Time delta of refresh request: delta={}s", delta_ts);
-                        if delta_ts < 0 && delta_ts > REFRESH_REQUEST_TIMEWINDOW {
-                            // get gpio pin as input and read state
-                            let state = get_state(&Gpio::new()?.get(GPIO_PIN_18)?.into_input_pullup());
-                            log::info!("{} Refresh state {:?}", Utc::now().timestamp(), state);
+                        if delta_ts < 0 && delta_ts > REFRESH_REQUEST_TIMEWINDOW_SECONDS {
+                            let command = sensor_refresh_request.r_cmd;
+                            match command {
+                                0 => {
+                                    // cmd => refresh
 
-                            // check user environment variable is set
-                            let user = get_user_from_env();
-                            if user == *USER_ID_ERROR { return Err("Couldn't get GOOGLE_USER_ID")? };
+                                    // get gpio pin as input and read state
+                                    let state = get_state(&Gpio::new()?.get(GPIO_PIN_18)?.into_input_pullup());
+                                    log::info!("{} Refresh state {:?}", Utc::now().timestamp(), state);
 
-                            // update (cloud) state and notify (Android) user
-                            // will only notify if state is different to the one in the cloud
-                            if let Err(error) = update_state_and_notify_user(user, state) {
-                                log::error!("Panic on update_state_and_notify_user {:?}", error);
-                                panic!("Error: {:?}", error);
+                                    // check user environment variable is set
+                                    let user : String = get_user_from_env();
+
+                                    // update (cloud) state and notify (Android) user
+                                    // will only notify if state is different to the one in the cloud
+                                    if let Err(error) = update_state_and_notify_user(user, state) {
+                                        log::error!("Panic on update_state_and_notify_user {:?}", error);
+                                        panic!("Error: {:?}", error);
+                                    }
+                                }
+                                1 => {
+                                    // cmd => status
+                                    log::info!("Refresh online state command request");
+
+                                    // setup firestore
+                                    let db = FirestoreDb::with_options_service_account_key_file(
+                                        FirestoreDbOptions::new(config_env_var("GOOGLE_PROJECT_ID")?.to_string()),
+                                        config_env_var("GOOGLE_APPLICATION_CREDENTIALS")?.to_string().into()
+                                    ).await?;
+                                    log::info!("Firestore DB initialized");
+                                    
+                                    // check user environment variable is set
+                                    let user : String = get_user_from_env();
+                                    let state : State = get_state(&Gpio::new()?.get(GPIO_PIN_18)?.into_input_pullup());
+                                    let s : String = match state {
+                                        State::Open => "OPEN".to_lowercase().to_string(),
+                                        State::Closed => "CLOSED".to_lowercase().to_string(),
+                                    };
+
+                                    // Update sensor document with online = true
+                                    db.fluent()
+                                    .update()
+                                    .in_col("sensors")
+                                    .document_id(user.clone())
+                                    .object(&SensorObject {
+                                        onine: true,
+                                        state: s
+                                    })
+                                    .execute()
+                                    .await?;
+                                    log::info!("Online state set to true");
+                                }
+                                _ => {
+                                    // not recognized
+                                    log::info!("Unknown command recevied!");
+                                }    
                             }
                         }
                     }
@@ -173,7 +226,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // listen for refresh requests
     fs_listener.await?;
-
+    
     // since we are starting up, and sensor state may have changed on device power-off
     // make a one time update and notify
     if let Err(error) = update_state_and_notify_user(get_user_from_env(), get_state(&sensor_door_pin)) {
